bb0d03f8cd0db7b2a00e5ec195f8b3d867e5433e
Edwin Alfais <94055465+edwin-isa@users.noreply.github.com>
2022-10-31 14:35:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// Search Box
+
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+    menu.classList.remove('active');
+}
+
+// Menu Box 
+let menu = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+    menu.classList.toggle('active');
+    search.classList.remove('active');
+}
+
+// Menyembunyikan Menu dan Search Box saat Scroll
+window.onscroll = () => {
+    menu.classList.remove('active');
+    search.classList.remove('active');
+}
+
+// Header
+let header = document.querySelector('header');
+
+window.addEventListener('scroll' , () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
